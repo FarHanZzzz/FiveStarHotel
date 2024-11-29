6ce8cac24b192b624714065b41aeeab98c239d99
FarHanZzzz <farhansad84@gmail.com>
2024-11-30 02:02:22 +0600

    16th commit

--- src/main/java/cse/fivestarhotel/HelloApplication.java
+++ src/main/java/cse/fivestarhotel/HelloApplication.java
@@ -1,18 +1,16 @@
 package cse.fivestarhotel;
-
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
-
 import java.io.IOException;
 
 public class HelloApplication extends Application {
     @Override
     public void start(Stage stage) throws IOException {
-        FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("/Login/Signin.fxml"));
-        Scene scene = new Scene(fxmlLoader.load());
-        stage.setTitle("Hello!");
+        FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("/cse/fivestarhotel/Login/Signin.fxml"));
+        Scene scene = new Scene(fxmlLoader.load(),1280,720);
+        stage.setTitle("Sign in!");
         stage.setScene(scene);
         stage.show();
     }
