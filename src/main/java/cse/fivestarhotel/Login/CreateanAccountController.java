dc1bd013fd69b15628b47cc289bea9d8a01290b0
JeninLOL <2330911@iub.edu.bd>
2024-11-30 03:14:34 +0600

    18th commit

--- src/main/java/cse/fivestarhotel/Login/CreateanAccountController.java
+++ src/main/java/cse/fivestarhotel/Login/CreateanAccountController.java
@@ -1,9 +1,16 @@
 package cse.fivestarhotel.Login;
 
+import cse.fivestarhotel.HelloApplication;
 import javafx.event.ActionEvent;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Node;
+import javafx.scene.Scene;
 import javafx.scene.control.DatePicker;
 import javafx.scene.control.PasswordField;
 import javafx.scene.control.TextField;
+import javafx.stage.Stage;
+
+import java.io.IOException;
 
 public class CreateanAccountController
 {
@@ -25,5 +32,15 @@ public class CreateanAccountController
 
     @javafx.fxml.FXML
     public void CreateAccountOnAction(ActionEvent actionEvent) {
+    }
+
+    @javafx.fxml.FXML
+    public void BackToGuestLoginOnAction(ActionEvent actionEvent) throws IOException {
+        FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("/cse/fivestarhotel/Login/GuestLogin.fxml"));
+        Scene scene = new Scene(fxmlLoader.load());
+        Stage stage = (Stage) (((Node) actionEvent.getSource()).getScene().getWindow());
+        stage.setTitle("Hello");
+        stage.setScene(scene);
+        stage.show();
     }
 }
