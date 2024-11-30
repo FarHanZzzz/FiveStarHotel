84e96b50ed6c4a319479aa8b8eb2bf8df5610994
FarHanZzzz <farhansad84@gmail.com>
2024-12-01 04:28:54 +0600

    1
    started Coding so our commit will start from scratch.

--- src/main/java/cse/fivestarhotel/FrontDeskStaff/GuestcheckinController.java
+++ src/main/java/cse/fivestarhotel/FrontDeskStaff/GuestcheckinController.java
@@ -1,7 +1,58 @@
 package cse.fivestarhotel.FrontDeskStaff;
 
+import javafx.event.ActionEvent;
+import javafx.scene.control.*;
+
 public class GuestcheckinController
 {
     @javafx.fxml.FXML
+    private TableColumn RoomtypeCol;
+    @javafx.fxml.FXML
+    private Label CheckInStatusLabel;
+    @javafx.fxml.FXML
+    private TableColumn MaritalStatusCol;
+    @javafx.fxml.FXML
+    private TableColumn NameCol;
+    @javafx.fxml.FXML
+    private TableColumn HowManyRoomsCol;
+    @javafx.fxml.FXML
+    private TableColumn idCol;
+    @javafx.fxml.FXML
+    private TextField GuestidTextField;
+    @javafx.fxml.FXML
+    private TextField GuestNameTextField;
+    @javafx.fxml.FXML
+    private TextField GuestContactNoTetField;
+    @javafx.fxml.FXML
+    private ComboBox<String> MaritalStatusComboBox;
+    @javafx.fxml.FXML
+    private TableColumn NationalityCol;
+    @javafx.fxml.FXML
+    private TableColumn ContactNoCol;
+    @javafx.fxml.FXML
+    private TextField HowManyRoomsTextField;
+    @javafx.fxml.FXML
+    private TextField GuestNationalityTextField;
+    @javafx.fxml.FXML
+    private ComboBox<String> RoomTypeComboBox;
+    @javafx.fxml.FXML
+    private TableView CheckinTableView;
+
+    @javafx.fxml.FXML
     public void initialize() {
-    }}
+
+
+
+    }
+
+    @javafx.fxml.FXML
+    public void ClearFieldsOnAction(ActionEvent actionEvent) {
+
+    }
+
+    @javafx.fxml.FXML
+    public void CheckInOnAction(ActionEvent actionEvent) {
+
+
+    }
+}
--- src/main/java/cse/fivestarhotel/HelloApplication.java
+++ src/main/java/cse/fivestarhotel/HelloApplication.java
@@ -19,3 +19,4 @@ public class HelloApplication extends Application {
         launch();
     }
 }
+
