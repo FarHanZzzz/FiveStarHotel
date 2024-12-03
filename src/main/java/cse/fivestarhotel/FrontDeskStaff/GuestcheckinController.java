996be9f11572081ce14715d603bc3a52f62899a0
FarHanZzzz <farhansad84@gmail.com>
2024-12-04 00:59:59 +0600

    8th commit

--- src/main/java/cse/fivestarhotel/FrontDeskStaff/GuestcheckinController.java
+++ src/main/java/cse/fivestarhotel/FrontDeskStaff/GuestcheckinController.java
@@ -2,91 +2,49 @@ package cse.fivestarhotel.FrontDeskStaff;
 
 import javafx.event.ActionEvent;
 import javafx.scene.control.*;
-import javafx.scene.control.cell.PropertyValueFactory;
-
-import java.io.Serializable;
-import java.util.ArrayList;
 
 public class GuestcheckinController
 {
     @javafx.fxml.FXML
-    private TableColumn<CheckinAssociation,String> RoomtypeCol;
+    private TableView CheckinTableView;
+    @javafx.fxml.FXML
+    private TableColumn NumberofRoomsCol;
+    @javafx.fxml.FXML
+    private TableColumn RoomtypeCol;
     @javafx.fxml.FXML
     private Label CheckInStatusLabel;
     @javafx.fxml.FXML
-    private TableColumn<CheckinAssociation,String> NameCol;
+    private TextField GuestEmailTextField;
+    @javafx.fxml.FXML
+    private TableColumn NameCol;
+    @javafx.fxml.FXML
+    private TextField NumberofRoomsTextField;
+    @javafx.fxml.FXML
+    private DatePicker CheckoutDatePicker;
     @javafx.fxml.FXML
     private TextField GuestNameTextField;
     @javafx.fxml.FXML
     private TextField GuestContactNoTetField;
     @javafx.fxml.FXML
+    private ComboBox GuestMaritalStatusComboBox;
+    @javafx.fxml.FXML
+    private TableColumn EmailCol;
+    @javafx.fxml.FXML
     private TextField GuestNationalityTextField;
     @javafx.fxml.FXML
     private ComboBox<String> RoomTypeComboBox;
-    @javafx.fxml.FXML
-    private TableView<CheckinAssociation> CheckinTableView;
-    @javafx.fxml.FXML
-    private TableColumn<CheckinAssociation,Integer> NumberofRoomsCol;
-    @javafx.fxml.FXML
-    private TextField NumberofRoomsTextField;
-    @javafx.fxml.FXML
-    private ComboBox<String> GuestMaritalStatusComboBox;
-    @javafx.fxml.FXML
-    private DatePicker CheckoutDatePicker;
-    @javafx.fxml.FXML
-    private TextField GuestEmailTextField;
-    @javafx.fxml.FXML
-    private TableColumn<CheckinAssociation,String> EmailCol;
-
-
-    ArrayList<CheckinAssociation> checkinassociationlist = new ArrayList<>();
-    ArrayList<Guest> guestlist = new ArrayList<>();
-    ArrayList<Room> roomlist = new ArrayList<>();
-
-
 
     @javafx.fxml.FXML
     public void initialize() {
-
-        NameCol.setCellValueFactory(new PropertyValueFactory<>("name"));
-        EmailCol.setCellValueFactory(new PropertyValueFactory<>("email"));
-        RoomtypeCol.setCellValueFactory(new PropertyValueFactory<>("roomtype"));
-        NumberofRoomsCol.setCellValueFactory(new PropertyValueFactory<>("NoofRooms"));
-
-
+        RoomTypeComboBox.getItems().addAll("helo","jony");
 
     }
 
-
-
-
     @javafx.fxml.FXML
     public void ClearFieldsOnAction(ActionEvent actionEvent) {
-
     }
 
     @javafx.fxml.FXML
     public void CheckInOnAction(ActionEvent actionEvent) {
-
-
-
-        for (int i =1 ; i <=50; i++){
-
-        }
-
-
-
-
-
-
-//        CheckinTableView.getItems().add()
-
-
-
-
-
-
-
-
     }
 }
